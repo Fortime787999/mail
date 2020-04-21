c25348887767ef1846ac32afd7b654e6004394e3
Fortime787999 <foreverxzy@protonmail.com>
2020-04-21 11:45:26 +0800

    配置搜索方案，检索引擎

--- mail/settings/dev.py
+++ mail/settings/dev.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'oauth',
     'areas',
     'goods',
+    'haystack', # 全文检索
 ]
 
 MIDDLEWARE = [
@@ -242,3 +243,18 @@ IMAGE_URL = 'http://image.meiduo.site:8888/'
 
 # 指定文件存储类
 DEFAULT_FILE_STORAGE = 'mail.utils.fastdfs.MailStorage.MailStorage'
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://192.168.131.128:9200/', # Elasticsearch服务器ip地址，端口号固定为9200
+        'INDEX_NAME': 'mail', # Elasticsearch建立的索引库的名称
+    },
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+# 每页显示商品数量
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5
